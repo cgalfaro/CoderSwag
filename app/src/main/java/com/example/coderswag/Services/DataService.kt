8e195efdf3007e2f0782dc4e21b8712d43c46074
Carlos Alfaro <carlosalfaro@finblok.tech>
2019-06-25 15:30:33 -0600

    Created product & category classes Added DataService as singleton

---
+++ app/src/main/java/com/example/coderswag/Services/DataService.kt
@@ -0,0 +1,36 @@
+package com.example.coderswag.Services
+
+import com.example.coderswag.Model.Category
+import com.example.coderswag.Model.Product
+
+object DataService {
+
+    val categories = listOf(
+        Category("SHIRTS", "SHIRTIMAGE"),
+        Category("HOODIES", "hoddieimage"),
+        Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage")
+    )
+
+    val hats = listOf(
+        Product("Devslopes Graphic Beanie", "$18", "hat01"),
+        Product("Devslopes Hat Black", "$20", "hat02"),
+        Product("Devslopes Hat White", "$25", "hat03"),
+        Product("Devslopes Hat Snapback", "$22", "hat04")
+    )
+
+    val hoddies = listOf(
+        Product("Devslopes Hoodie Black", "$28", "hoodie01"),
+        Product("Devslopes Hoodie Red", "$32", "hoodie02"),
+        Product("Devslopes Gray Hoodie", "$45", "hoodie03"),
+        Product("Devslopes Black Hoodie", "$35", "hoodie04")
+    )
+
+    val shirts = listOf(
+        Product("Devslopes Shirt Black", "$18", "shirt01"),
+        Product("Devslopes Badge Light Gray", "$15", "shirt02"),
+        Product("Devslopes Logo Shirt red", "$22", "shirt03"),
+        Product("Devslopes Hustle", "$22", "shirt04"),
+        Product("Kickflip Studios", "$18", "shirt05")
+    )
+}
